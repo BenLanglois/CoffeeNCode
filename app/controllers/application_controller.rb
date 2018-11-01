8bf2099e4e0c5f24ebcd137585f98247c712908e
Ben Langlois <blanglois000@gmail.com>
2018-10-31 21:11:00 -0400

    Completed Lesson 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-
+    # protect_from_forgery with: :exception
+    #
     # def hello # added our own function to say something friendly
     #     render html: "hello, world!"
     # end
