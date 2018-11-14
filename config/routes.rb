a1a0b1087a53851a552b3abbcf30ba01186ee4fe
Ben Langlois <blanglois000@gmail.com>
2018-11-14 10:39:00 -0500

    Lesson 4 completed

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     get  '/signup',  to: 'users#new'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     # root 'application#hello'
+    resources :users
 end
